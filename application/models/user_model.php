39030d89a940e286ad14081819137767f115844d
4kari <mypasshidden@gmail.com>
2021-10-08 16:37:55 +0700

    update add mhs

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -69,29 +69,17 @@ class user_model extends CI_Model
     {
         $nim=$this->input->post('nim');
         $nama=$this->input->post('nama');
+        
         if($nim && $nama){
-                // json_decode($this->curl->simple_post('http://10.5.12.26/user/api/Mahasiswa/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
-                var_dump(json_decode($this->curl->simple_post('http://localhost/microservice/user/api/Mahasiswa/',$data,array(CURLOPT_BUFFERSIZE => 10)),true));
-                echo "mahasiswa ditambahkan";
+            $data = [
+                'nim'=>$nim,
+                'nama'=>$nama
+            ];
+            // json_decode($this->curl->simple_post('http://10.5.12.26/user/api/Mahasiswa/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
+            json_decode($this->curl->simple_post('http://localhost/microservice/user/api/Mahasiswa/',$data,array(CURLOPT_BUFFERSIZE => 10)),true);
+            echo "mahasiswa ditambahkan";
         }else{
             echo "data kosong";
         }
-    }
-
-    public function getUserByLv($lvl1='null',$lvl2='null',$lvl3='null',$lvl4='null' ){
-        $query ="SELECT u.id,  u.username, u.level_id, l.level as level FROM user u, user_level l WHERE u.level_id = l.id and
-        (u.level_id=$lvl1 OR u.level_id=$lvl2 OR u.level_id=$lvl3 OR u.level_id=$lvl4)";
-        return $this->db->query($query)->result_array();
-    }
-    public function getUserByProdi($level, $prodi){
-        if ($level==3){
-            $query ="SELECT u.id,  u.username, u.level_id, l.level as level FROM user u, user_level l, dosen d WHERE u.level_id = l.id AND d.username = u.id AND d.prodi=$prodi AND u.level_id=$level  
-            ORDER BY `u`.`username` ASC";
-        }
-        if ($level==4){
-            $query ="SELECT u.id,  u.username, u.level_id, l.level as level FROM user u, user_level l, mahasiswa m WHERE u.level_id = l.id AND m.username = u.id AND m.prodi=$prodi AND u.level_id=$level  
-            ORDER BY `u`.`username` ASC";
-        }
-        return $this->db->query($query)->result_array();
     }
 }
