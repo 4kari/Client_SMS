cdfb85f7ed0a8965fa6376fe61ac522bfde549ae
4kari <mypasshidden@gmail.com>
2021-10-20 19:15:37 +0700

    bimbingan data percobaan

--- application/views/mahasiswa/Bimbingan.php
+++ application/views/mahasiswa/Bimbingan.php
@@ -1,34 +1,41 @@
 <?php
-$topik=[
-  ['id'=>'0', 'topik'=>"Rekayasa Perangkat Lunak"],
-  ['id'=>'1', 'topik'=>"Citra" ],
-  ['id'=>'2', 'topik'=>"Komputasi" ],
+$post=[
+  ['id'=>'0', 'judul'=>"Rekayasa Perangkat Lunak", 'file'=>'coba.pdf', 'tipe' => 1, 'tanggal_dibuat'=>'19/10/2021'],
+  ['id'=>'1', 'judul'=>"Rekayasa Perangkat Lunak", 'file'=>'coba.pdf', 'tipe' => 1, 'tanggal_dibuat'=>'19/10/2021'],
+  ['id'=>'2', 'judul'=>"Rekayasa Perangkat Lunak", 'file'=>'coba.pdf', 'tipe' => 1, 'tanggal_dibuat'=>'19/10/2021']
 ];
-$Dosbing1=[
-  ['id'=>'0', 'dosen'=>"Dosen A"],
-  ['id'=>'1', 'dosen'=>"Dosen B" ],
-  ['id'=>'2', 'dosen'=>"Dosen c" ],
-];
-$Dosbing2=[
-  ['id'=>'0', 'dosen'=>"Dosen A"],
-  ['id'=>'1', 'dosen'=>"Dosen B" ],
-  ['id'=>'2', 'dosen'=>"Dosen c" ],
-];
-
 ?>
         <!-- Begin Page Content -->
         <div class="container-fluid">
 
           <!-- Page Heading -->
           <h1 class="h3 mb-2 text-gray-800"><?=$judul;?></h1>
-          <p class="mb-4">halaman ini digunakan untuk mengajukan topik yang akan dibahas pada skripsi</p>
+          <p class="mb-4">halaman ini digunakan umtuk melakukan proses bimbingan skripsi</p>
           <!-- DataTales Example -->
-          <div class="card shadow mb-4">
+          <div class="card shadow mb-4 h-75">
             <div class="card-header py-3">
-              <h6 class="m-0 font-weight-bold text-primary">Pengajuan Topik</h6>
+              <h6 class="m-0 font-weight-bold text-primary">Bimbingan Skripsi</h6>
             </div>
             <div class="card-body">
-
+              <?php if (empty($post)){ ?>
+                <div id="primary" class="content-area">
+                <div class="layout social"> 
+                  <div class="container-fluid">
+                    <div class="row">
+                      <div class="col-lg-8 col-main">
+                        <main id="main" class="main-content">
+                          <div class="beehive-title-bar social">
+                            <div class="title-bar-wrapper">
+                              <div class="title-wrapper screen-reader-text">
+                                <div class="alert alert-danger" role="alert">
+                                  Data not found!
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </main><!-- #main -->
+                      </div><!-- .col-main -->
+              <?php }else{ ?>
               <div id="primary" class="content-area">
                 <div class="layout social"> 
                   <div class="container-fluid">
@@ -38,43 +45,34 @@ $Dosbing2=[
                           <div class="beehive-title-bar social">
                             <div class="title-bar-wrapper">
                               <div class="title-wrapper screen-reader-text">
-                                <h1 class="title h3">Activity</h1>
+                                <h1 class="title h3">Bimbingan Skripsi</h1>
                               </div>
                             </div>
                           </div>
                           <article id="post-0" class="bp_activity type-bp_activity post-0 page type-page status-publish hentry beehive-post">
                             <div class="entry-content clearfix">
                               <div id="buddypress" class="buddypress-wrap beehive bp-dir-hori-nav alignwide">
-                                <nav class="activity-type-navs main-navs bp-navs dir-navs " role="navigation" aria-label="Directory menu">
-                                  <ul class="component-navigation activity-nav">
-                                    <li id="activity-all" class="dynamic"  data-bp-scope="all" data-bp-object="activity">
-                                      <a href="https://mythemestore.com/beehive-preview/activity/">
-                                        All Members
-                                        <span class="count">0</span>
-                                      </a>
-                                    </li>
-                                  </ul><!-- .component-navigation -->
-                                </nav><!-- .bp-navs -->
                                 <div class="screen-content">
                                   <div id="activity-stream" class="activity" data-bp-list="activity" style="display:block">
                                     <ul class="activity-list item-list bp-list">
                                       <li class="groups bbp_reply_create activity-item date-recorded-1628175393 animate-item slideInUp" id="activity-7124" data-bp-activity-id="7124" data-bp-timestamp="1628175393" style="visibility: visible; animation-name: slideInUp;">
                                         <div class="activity-avatar item-avatar">
                                           <a href="https://mythemestore.com/beehive-preview/members/user/">
-                                            <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508a58db-bpfull.jpg" class="avatar user-3-avatar avatar-200 photo" width="200" height="200" alt="Profile picture of John Doe">
+                                            <!-- <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508a58db-bpfull.jpg" class="avatar user-3-avatar avatar-200 photo" width="200" height="200" alt="Profile picture of John Doe"> -->
                                           </a>
                                         </div>
                                         <div class="activity-content">
                                           <div class="activity-header">
                                             <div class="posted-meta">
-                                              <p><a href="https://mythemestore.com/beehive-preview/members/user/">John Doe</a> replied to the topic <a href="https://mythemestore.com/beehive-preview/groups/backpakers-club/forum/topic/test-me-topic/"><img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/group-avatars/4/5e2cc339c9d04-bpthumb.jpg" class="avatar group-4-avatar avatar-20 photo" width="20" height="20" alt="Group logo of Backpakers Club">Test me topic</a> in the forum <a href="https://mythemestore.com/beehive-preview/groups/backpakers-club/forum/">Backpakers Club</a></p>
+                                              <p>judul skripsi ; <?= $post[0]['judul'];?> <br>file : unduh file <a href="#">di sini</a></p>
                                             </div>
                                             <div class="date mute">
-                                              1 hour, 2 minutes ago
+                                              Diunggah pada <?=$post[0]['tanggal_dibuat'];?>
                                             </div>
                                           </div>
+                                          <hhr>
                                           <div class="activity-inner">
-                                            <p>几哦哦</p>
+                                            <p>pesan 1</p>
                                           </div>
                                         </div>
                                         
@@ -83,7 +81,7 @@ $Dosbing2=[
                                             <li id="acomment-6656" class="comment-item" data-bp-activity-comment-id="6656">
                                               <div class="acomment-avatar item-avatar">
                                                 <a href="https://mythemestore.com/beehive-preview/members/user/">
-                                                  <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508b3ce1-bpthumb.jpg" class="avatar user-3-avatar avatar-50 photo" width="50" height="50" alt="Profile picture of John Doe">		</a>
+                                                  <!-- <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508b3ce1-bpthumb.jpg" class="avatar user-3-avatar avatar-50 photo" width="50" height="50" alt="Profile picture of John Doe">		</a> -->
                                               </div>
                                               <div class="acomment-meta">
                                                 <a href="https://mythemestore.com/beehive-preview/members/user/">John Doe</a> replied <a href="https://mythemestore.com/beehive-preview/activity/p/202/#acomment-6656" class="activity-time-since"><time class="time-since" datetime="2021-06-30 02:23:48" data-bp-timestamp="1625019828">1 month ago</time></a>
@@ -101,7 +99,7 @@ $Dosbing2=[
                                             <li id="acomment-6679" class="comment-item" data-bp-activity-comment-id="6679">
                                               <div class="acomment-avatar item-avatar">
                                                 <a href="https://mythemestore.com/beehive-preview/members/user/">
-                                                  <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508b3ce1-bpthumb.jpg" class="avatar user-3-avatar avatar-50 photo" width="50" height="50" alt="Profile picture of John Doe">		</a>
+                                                  <!-- <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508b3ce1-bpthumb.jpg" class="avatar user-3-avatar avatar-50 photo" width="50" height="50" alt="Profile picture of John Doe">		</a> -->
                                               </div>
                                               <div class="acomment-meta">
                                                 <a href="https://mythemestore.com/beehive-preview/members/user/">John Doe</a> replied <a href="https://mythemestore.com/beehive-preview/activity/p/202/#acomment-6679" class="activity-time-since"><time class="time-since" datetime="2021-07-02 15:01:13" data-bp-timestamp="1625238073">1 month ago</time></a>
@@ -118,7 +116,7 @@ $Dosbing2=[
                                                 <li id="acomment-7018" class="comment-item" data-bp-activity-comment-id="7018">
                                                   <div class="acomment-avatar item-avatar">
                                                     <a href="https://mythemestore.com/beehive-preview/members/user/">
-                                                      <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508b3ce1-bpthumb.jpg" class="avatar user-3-avatar avatar-50 photo" width="50" height="50" alt="Profile picture of John Doe">		</a>
+                                                      <!-- <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508b3ce1-bpthumb.jpg" class="avatar user-3-avatar avatar-50 photo" width="50" height="50" alt="Profile picture of John Doe">		</a> -->
                                                   </div>
                                                   <div class="acomment-meta">
                                                     <a href="https://mythemestore.com/beehive-preview/members/user/">John Doe</a> replied <a href="https://mythemestore.com/beehive-preview/activity/p/202/#acomment-7018" class="activity-time-since"><time class="time-since" datetime="2021-07-30 09:20:00" data-bp-timestamp="1627636800">6 days, 6 hours ago</time></a>
@@ -137,7 +135,7 @@ $Dosbing2=[
                                             <li id="acomment-6702" class="comment-item" data-bp-activity-comment-id="6702">
                                               <div class="acomment-avatar item-avatar">
                                                 <a href="https://mythemestore.com/beehive-preview/members/user/">
-                                                  <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508b3ce1-bpthumb.jpg" class="avatar user-3-avatar avatar-50 photo" width="50" height="50" alt="Profile picture of John Doe">		
+                                                  <!-- <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508b3ce1-bpthumb.jpg" class="avatar user-3-avatar avatar-50 photo" width="50" height="50" alt="Profile picture of John Doe">		 -->
                                                 </a>
                                               </div>
                                               <div class="acomment-meta">
@@ -155,7 +153,7 @@ $Dosbing2=[
                                             <li id="acomment-7046" class="comment-item" data-bp-activity-comment-id="7046">
                                               <div class="acomment-avatar item-avatar">
                                                 <a href="https://mythemestore.com/beehive-preview/members/user/">
-                                                  <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508b3ce1-bpthumb.jpg" class="avatar user-3-avatar avatar-50 photo" width="50" height="50" alt="Profile picture of John Doe">		</a>
+                                                  <!-- <img loading="lazy" src="https://mythemestore.com/beehive-preview/wp-content/uploads/avatars/3/61012508b3ce1-bpthumb.jpg" class="avatar user-3-avatar avatar-50 photo" width="50" height="50" alt="Profile picture of John Doe">		</a> -->
                                               </div>
                                               <div class="acomment-meta">
                                                 <a href="https://mythemestore.com/beehive-preview/members/user/">John Doe</a> replied <a href="https://mythemestore.com/beehive-preview/activity/p/202/#acomment-7046" class="activity-time-since"><time class="time-since" datetime="2021-08-01 05:09:36" data-bp-timestamp="1627794576">4 days, 10 hours ago</time></a>
@@ -174,9 +172,9 @@ $Dosbing2=[
                           </article><!-- #post-0 -->
                         </main><!-- #main -->
                       </div><!-- .col-main -->
-
+                        <?php } ?>
                       <div class="col-lg-4 col-aside">
-                        <aside id="buddypress_sidebar" class="widget-area sidebar-widget-area sticky-sidebar position-fixed">
+                        <aside id="buddypress_sidebar" class="widget-area sidebar-widget-area sticky-sidebar position-fixed bg-white">
                           <div id="bp_core_members_widget-2" class="widget widget_bp_core_members_widget buddypress widget">
                             <h5 class="widget-title">Members</h5>
                               <form>
