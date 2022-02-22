e01d36f8309e2d15f11afd1c1362685e24d7f039
4kari <mypasshidden@gmail.com>
2022-02-22 13:51:06 +0700

    update public

--- application/models/koordinator_model.php
+++ application/models/koordinator_model.php
@@ -3,15 +3,15 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class koordinator_model extends CI_Model
 {
-    protected $ipSkripsi='http://10.5.12.24/skripsi/api/';
-    protected $ipPenjadwalan='http://10.5.12.82/penjadwalan/api/';
-    protected $ipDiskusi='http://10.5.12.56/diskusi/api/';
-    protected $ipUser='http://10.5.12.18/user/api/';
+    // protected $ipSkripsi='http://10.5.12.24/skripsi/api/';
+    // protected $ipPenjadwalan='http://10.5.12.82/penjadwalan/api/';
+    // protected $ipDiskusi='http://10.5.12.56/diskusi/api/';
+    // protected $ipUser='http://10.5.12.18/user/api/';
 
-    // protected $ipSkripsi='http://localhost/microservice/skripsi/api/';
-    // protected $ipPenjadwalan='http://localhost/microservice/penjadwalan/api/';
-    // protected $ipDiskusi='http://localhost/microservice/diskusi/api/';
-    // protected $ipUser='http://localhost/microservice/user/api/';
+    protected $ipSkripsi='http://localhost:8080/microservice/skripsi/api/';
+    protected $ipPenjadwalan='http://localhost:8080/microservice/penjadwalan/api/';
+    protected $ipDiskusi='http://localhost:8080/microservice/diskusi/api/';
+    protected $ipUser='http://localhost:8080/microservice/user/api/';
     
     public function getDosen()
     {
